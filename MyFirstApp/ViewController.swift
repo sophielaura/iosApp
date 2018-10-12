c1cb033eddff1d8a1e159482af2e1d92100b0b9a
User23 <user23@user23de-imac.local>
2018-10-12 13:45:53 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by User23 on 2018/9/12.
+//  Copyright © 2018年 User23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*@IBOutlet weak var myWeb: UIWebView!
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath){
+    
+        //code to execute on click
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            
+            let urlPath:String = "http://www.youtube.com.tw"
+        let url:NSURL = NSURL(string: urlPath)!
+        let request:NSURLRequest = NSURLRequest(URL: url)
+            
+        self.myWeb.loadRequest(request as URLRequest)
+        
+    }*/
+    
+
+}
+
